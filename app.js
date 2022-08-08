fafc75dc6357f4a0a06cbaa953e873d91a3e3283
Nicholas Ryder <nryder7@gmail.com>
2022-08-08 11:53:08 -0400

    update index.html add browser-app.js

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const productRouter = require('./routes/productRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const orderRouter = require('./routes/orderRoutes');
 
-const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 const rateLimiter = require('express-rate-limit');
@@ -32,16 +31,10 @@ app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
 
-app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(fileUpload());
 app.use(express.static('./public'));
-
-app.get('/', (req, res) => {
-  res.status(200).send({ msg: 'ok' });
-  // console.log(req.signedCookies);
-});
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
